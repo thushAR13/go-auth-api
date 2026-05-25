7b7c97c006aed5de82b0d5b8268f7fd99c946f03
Thushar Kulal <thushar@Thushars-MacBook-Air.local>
2026-05-25 19:29:34 +0530

    Removed the space in header validation

--- middleware/auth.go
+++ middleware/auth.go
@@ -15,7 +15,7 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		authHeader := r.Header.Get("Authorization")
-		if authHeader == " " {
+		if authHeader == "" {
 			http.Error(w, "Authorization header required", http.StatusUnauthorized)
 			return
 		}
